534e112372cdb014c69fc9077621687149671136
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-11-28 11:34:25 +0200

    Add files via upload

--- AD/Form1.Designer.cs
+++ AD/Form1.Designer.cs
@@ -31,9 +31,15 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lisa_kat = new System.Windows.Forms.Button();
             this.lbl_hind = new System.Windows.Forms.Label();
             this.txt_box = new System.Windows.Forms.TextBox();
+            this.lbl_kogus = new System.Windows.Forms.Label();
+            this.txt_kogus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_toode = new System.Windows.Forms.TextBox();
+            this.lisa_btn = new System.Windows.Forms.Button();
+            this.kustuta = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,15 +68,15 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Kategooriad\r\n";
             // 
-            // button1
+            // lisa_kat
             // 
-            this.button1.Location = new System.Drawing.Point(196, 256);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Lisa Kategooria\r\n";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.lisa_kat.Location = new System.Drawing.Point(196, 256);
+            this.lisa_kat.Name = "lisa_kat";
+            this.lisa_kat.Size = new System.Drawing.Size(116, 23);
+            this.lisa_kat.TabIndex = 3;
+            this.lisa_kat.Text = "Lisa Kategooria\r\n";
+            this.lisa_kat.UseVisualStyleBackColor = true;
+            this.lisa_kat.Click += new System.EventHandler(this.lisa_kat_Click);
             // 
             // lbl_hind
             // 
@@ -88,12 +94,70 @@
             this.txt_box.Size = new System.Drawing.Size(116, 20);
             this.txt_box.TabIndex = 5;
             // 
+            // lbl_kogus
+            // 
+            this.lbl_kogus.AutoSize = true;
+            this.lbl_kogus.Location = new System.Drawing.Point(100, 184);
+            this.lbl_kogus.Name = "lbl_kogus";
+            this.lbl_kogus.Size = new System.Drawing.Size(37, 13);
+            this.lbl_kogus.TabIndex = 6;
+            this.lbl_kogus.Text = "Kogus";
+            // 
+            // txt_kogus
+            // 
+            this.txt_kogus.Location = new System.Drawing.Point(196, 181);
+            this.txt_kogus.Name = "txt_kogus";
+            this.txt_kogus.Size = new System.Drawing.Size(116, 20);
+            this.txt_kogus.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Toode nimetus";
+            // 
+            // txt_toode
+            // 
+            this.txt_toode.Location = new System.Drawing.Point(196, 142);
+            this.txt_toode.Name = "txt_toode";
+            this.txt_toode.Size = new System.Drawing.Size(116, 20);
+            this.txt_toode.TabIndex = 9;
+            // 
+            // lisa_btn
+            // 
+            this.lisa_btn.Location = new System.Drawing.Point(349, 291);
+            this.lisa_btn.Name = "lisa_btn";
+            this.lisa_btn.Size = new System.Drawing.Size(108, 23);
+            this.lisa_btn.TabIndex = 10;
+            this.lisa_btn.Text = "Lisa";
+            this.lisa_btn.UseVisualStyleBackColor = true;
+            this.lisa_btn.Click += new System.EventHandler(this.lisa_btn_Click);
+            // 
+            // kustuta
+            // 
+            this.kustuta.Location = new System.Drawing.Point(349, 256);
+            this.kustuta.Name = "kustuta";
+            this.kustuta.Size = new System.Drawing.Size(108, 23);
+            this.kustuta.TabIndex = 11;
+            this.kustuta.Text = "Kustuta Kategooria";
+            this.kustuta.UseVisualStyleBackColor = true;
+            this.kustuta.Click += new System.EventHandler(this.kustuta_Click);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1015, 540);
+            this.Controls.Add(this.kustuta);
+            this.Controls.Add(this.lisa_btn);
+            this.Controls.Add(this.txt_toode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_kogus);
+            this.Controls.Add(this.lbl_kogus);
             this.Controls.Add(this.txt_box);
             this.Controls.Add(this.lbl_hind);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lisa_kat);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.dataGridView2);
@@ -112,9 +176,15 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button lisa_kat;
         private System.Windows.Forms.Label lbl_hind;
         private System.Windows.Forms.TextBox txt_box;
+        private System.Windows.Forms.Label lbl_kogus;
+        private System.Windows.Forms.TextBox txt_kogus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_toode;
+        private System.Windows.Forms.Button lisa_btn;
+        private System.Windows.Forms.Button kustuta;
     }
 }
 
--- AD/Form1.cs
+++ AD/Form1.cs
@@ -13,7 +13,7 @@ namespace AD
 {
     public partial class Form1 : Form
     {
-        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|AppData\DataDirectory|\tooded_DB.mdf;Integrated Security=True");
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\opilane\Source\Repos\Matvei Kulakovski\AD\AD\AppData\tooded_DB.mdf"";Integrated Security=True");
         SqlDataAdapter adapter_toode, adapter_kategooria;
         SqlCommand comand;
         public Form1()
@@ -36,33 +36,97 @@ namespace AD
             connect.Close();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void lisa_kat_Click(object sender, EventArgs e)
         {
-            comand = new SqlCommand("INSERT INTO KategooriaTable (Kategooria_nimetus) VALUES (@Kat)", connect);
-            connect.Open();
-            comand.Parameters.AddWithValue("@Kat", comboBox2.Text);
-            comand.ExecuteNonQuery();
-            connect.Close();
-            comboBox2.Items.Clear();
-            NaitaKategooriad();
+            bool on = false;
+            foreach (var item in comboBox2.Items)
+                if (item.ToString() == comboBox2.Text)
+                {
+                    on = true;
+                }
+            if(on == false)
+            {
+                comand = new SqlCommand("INSERT INTO KategooriaTable (Kategooria_nimetus) VALUES (@Kat)", connect);
+                connect.Open();
+                comand.Parameters.AddWithValue("@Kat", comboBox2.Text);
+                comand.ExecuteNonQuery();
+                connect.Close();
+                comboBox2.Items.Clear();
+                NaitaKategooriad();
+            }
         }
+
+        
 
         public void NaitaAndmed()
         {
             connect.Open();
+            ComboBox cb1 = new ComboBox();
             DataTable dt_Toode = new DataTable();
-            adapter_toode = new SqlDataAdapter("SELECT * FROM ToodeTabel", connect);
+            adapter_toode = new SqlDataAdapter("SELECT * FROM ToodeTabel INNER JOIN KategooriaTable on ToodeTabel.Kategooriad=KategooriaTable.Id", connect);
+            //INNER JOIN KategooriaTable on ToodeTabel.Kattegooriad=KategooriaTable.Id
             adapter_toode.Fill(dt_Toode);
-            dataGridView1.DataSource = dt_Toode;
+            dataGridView2.DataSource = dt_Toode;
+            DataSet ds = new DataSet();
+            adapter_toode.Fill(ds);
+            cb1.DataSource = ds.Tables["KategooriaTable"];
+            cb1.DisplayMember = ("Kategooriad");
+            cb1.ValueMember = ("Kategooria_nimetus");
             connect.Close();
+        }
+
+        private void lisa_btn_Click(object sender, EventArgs e)
+        {
+            if (txt_toode.Text.Trim()!=string.Empty && txt_kogus.Text.Trim()!=string.Empty && txt_box.Text.Trim()!=string.Empty && comboBox2.SelectedItem!=null)
+            {
+                try
+                {
+                    connect.Open();
+                    comand = new SqlCommand("SELECT Id FROM KategooriaTable WHERE Kategooria_nimetus=@kat", connect);
+                    comand.Parameters.AddWithValue("@kat", comboBox2.Text);
+                    comand.ExecuteNonQuery();
+                    int Id = Convert.ToInt32(comand.ExecuteScalar());
+
+                    comand = new SqlCommand("INSERT INTO ToodeTabel (Toodenimetus, Kogus, Hind, Pilt, Kategooriad) VALUES(@toode, @kogus, @hind, @pilt, @kat)", connect);
+                    comand.Parameters.AddWithValue("@toode", txt_toode.Text);
+                    comand.Parameters.AddWithValue("@kogus", txt_kogus.Text);
+                    comand.Parameters.AddWithValue("@hind", txt_box.Text);
+                    comand.Parameters.AddWithValue("@pilt", txt_toode.Text + ".jpg");
+                    comand.Parameters.AddWithValue("@kat", Id);//id
+                    comand.ExecuteNonQuery();
+                    connect.Close();
+                    NaitaAndmed();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Andmebaasiga viga!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmed");
+            }
+        }
+
+        private void kustuta_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                string val_kat = comboBox2.SelectedItem.ToString();
+                comand = new SqlCommand("DELETE FROM KategooriaTable WHERE Kategooria_nimetus = @Kat", connect);
+                connect.Open();
+                comand.Parameters.AddWithValue("@Kat", val_kat);
+                comand.ExecuteNonQuery();
+                connect.Close();
+                comboBox2.Items.Clear();
+                NaitaKategooriad();
+            }
         }
 
         public void NaitaHind()
         {
             connect.Open();
-            DataTable dt_Toode = new DataTable();
             adapter_toode = new SqlDataAdapter("SELECT Hind FROM ToodeTabel", connect);
-            adapter_toode.Fill(dt_Toode);
 
             connect.Close();
         }
