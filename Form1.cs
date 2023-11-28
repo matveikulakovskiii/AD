09ee4c04e15500deb84d9c1f84d01700e7ec3486
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-11-28 11:31:11 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -38,13 +38,22 @@ namespace AD
 
         private void lisa_kat_Click(object sender, EventArgs e)
         {
-            comand = new SqlCommand("INSERT INTO KategooriaTable (Kategooria_nimetus) VALUES (@Kat)", connect);
-            connect.Open();
-            comand.Parameters.AddWithValue("@Kat", comboBox2.Text);
-            comand.ExecuteNonQuery();
-            connect.Close();
-            comboBox2.Items.Clear();
-            NaitaKategooriad();
+            bool on = false;
+            foreach (var item in comboBox2.Items)
+                if (item.ToString() == comboBox2.Text)
+                {
+                    on = true;
+                }
+            if(on == false)
+            {
+                comand = new SqlCommand("INSERT INTO KategooriaTable (Kategooria_nimetus) VALUES (@Kat)", connect);
+                connect.Open();
+                comand.Parameters.AddWithValue("@Kat", comboBox2.Text);
+                comand.ExecuteNonQuery();
+                connect.Close();
+                comboBox2.Items.Clear();
+                NaitaKategooriad();
+            }
         }
 
         
@@ -52,10 +61,17 @@ namespace AD
         public void NaitaAndmed()
         {
             connect.Open();
+            ComboBox cb1 = new ComboBox();
             DataTable dt_Toode = new DataTable();
-            adapter_toode = new SqlDataAdapter("SELECT * FROM ToodeTabel", connect);
+            adapter_toode = new SqlDataAdapter("SELECT * FROM ToodeTabel INNER JOIN KategooriaTable on ToodeTabel.Kategooriad=KategooriaTable.Id", connect);
+            //INNER JOIN KategooriaTable on ToodeTabel.Kattegooriad=KategooriaTable.Id
             adapter_toode.Fill(dt_Toode);
             dataGridView2.DataSource = dt_Toode;
+            DataSet ds = new DataSet();
+            adapter_toode.Fill(ds);
+            cb1.DataSource = ds.Tables["KategooriaTable"];
+            cb1.DisplayMember = ("Kategooriad");
+            cb1.ValueMember = ("Kategooria_nimetus");
             connect.Close();
         }
 
@@ -66,13 +82,17 @@ namespace AD
                 try
                 {
                     connect.Open();
+                    comand = new SqlCommand("SELECT Id FROM KategooriaTable WHERE Kategooria_nimetus=@kat", connect);
+                    comand.Parameters.AddWithValue("@kat", comboBox2.Text);
+                    comand.ExecuteNonQuery();
+                    int Id = Convert.ToInt32(comand.ExecuteScalar());
+
                     comand = new SqlCommand("INSERT INTO ToodeTabel (Toodenimetus, Kogus, Hind, Pilt, Kategooriad) VALUES(@toode, @kogus, @hind, @pilt, @kat)", connect);
                     comand.Parameters.AddWithValue("@toode", txt_toode.Text);
                     comand.Parameters.AddWithValue("@kogus", txt_kogus.Text);
                     comand.Parameters.AddWithValue("@hind", txt_box.Text);
                     comand.Parameters.AddWithValue("@pilt", txt_toode.Text + ".jpg");
-                    comand.Parameters.AddWithValue("@kat", comboBox2.SelectedIndex);//id
-
+                    comand.Parameters.AddWithValue("@kat", Id);//id
                     comand.ExecuteNonQuery();
                     connect.Close();
                     NaitaAndmed();
@@ -81,6 +101,25 @@ namespace AD
                 {
                     MessageBox.Show("Andmebaasiga viga!");
                 }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmed");
+            }
+        }
+
+        private void kustuta_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                string val_kat = comboBox2.SelectedItem.ToString();
+                comand = new SqlCommand("DELETE FROM KategooriaTable WHERE Kategooria_nimetus = @Kat", connect);
+                connect.Open();
+                comand.Parameters.AddWithValue("@Kat", val_kat);
+                comand.ExecuteNonQuery();
+                connect.Close();
+                comboBox2.Items.Clear();
+                NaitaKategooriad();
             }
         }
 
